1c331b0f5cf6faddf786954a29534009e1a872fa
amogh2019 <amogh1@outlook.com>
2022-08-06 19:21:19 +0530

    creating blog item  // mongo entity

---
+++ blog/server/blog_item.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	pb "github.com/amogh2019/blog-service/blog/proto"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type BlogItem struct {
+	ID       primitive.ObjectID `bson:"_id, omitempty"`
+	AuthorId string             `bson:"author_id"`
+	Title    string             `bson:"title"`
+	Content  string             `bson:"content"`
+}
+
+func documentToBlogDto(data *BlogItem) *pb.BlogDto {
+	return &pb.BlogDto{
+		Id:       data.ID.Hex(),
+		AuthorId: data.AuthorId,
+		Title:    data.Title,
+		Content:  data.Content,
+	}
+}
