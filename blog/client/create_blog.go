dc6bf27f5e6f5d2b60039da65d2a2512df184d14
amogh2019 <amogh1@outlook.com>
2022-08-07 16:25:33 +0530

    adding blog server client // calling create blog service

---
+++ blog/client/create_blog.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"bufio"
+	"context"
+	"log"
+
+	pb "github.com/amogh2019/blog-service/blog/proto"
+	"google.golang.org/grpc/status"
+)
+
+func CreateBlog(blogServiceClient *pb.BlogServiceClient, scanner *bufio.Scanner) {
+
+	log.Println("Preparing Create Blog Payload")
+
+	createBlogDto := pb.BlogDto{}
+	log.Println("Enter Blog Title")
+	(*scanner).Scan()
+	createBlogDto.Title = (*scanner).Text()
+	log.Println("Enter Blog Content")
+	(*scanner).Scan()
+	createBlogDto.Content = (*scanner).Text()
+	log.Println("Enter Blog Author")
+	(*scanner).Scan()
+	createBlogDto.AuthorId = (*scanner).Text()
+
+	blogIdDto, err := (*blogServiceClient).CreateBlog(context.Background(), &createBlogDto)
+	if err != nil {
+		grpcParsedStatus, ok := status.FromError(err)
+		if ok {
+			log.Println("ErrorCode : ", grpcParsedStatus.Code(), " ErrorMsg: ", grpcParsedStatus.Message())
+		} else {
+			log.Fatal("Non-gRPC Error in finding sqaure root from server", err)
+		}
+		return
+	}
+
+	createBlogDto.Id = blogIdDto.BlogId
+	log.Println("Blog Created. BlogId : ", blogIdDto.BlogId)
+}
---
+++ blog/client/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"bufio"
+	"log"
+	"os"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/credentials/insecure"
+
+	pb "github.com/amogh2019/blog-service/blog/proto"
+)
+
+var serverAddress string = "localhost:50052"
+
+func main() {
+
+	// setting up ssl for client
+	tlsEnabled := true
+	opts := []grpc.DialOption{}
+	if tlsEnabled {
+		cred, err := credentials.NewClientTLSFromFile("ssl/ca.crt", "")
+		if err != nil {
+			log.Fatal("error in loading CA trust certificate", err)
+		}
+		opts = append(opts, grpc.WithTransportCredentials(cred))
+	} else {
+		opts = append(opts, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	}
+
+	// setting up connection to server
+	conn, err := grpc.Dial(serverAddress, opts...)
+	if err != nil {
+		log.Fatal("error in starting dialup", serverAddress, err) // TODO this is not breaking when server is down // check how to write correct code to block this when server is not up
+	}
+	defer conn.Close()
+
+	client := pb.NewBlogServiceClient(conn)
+
+	scanner := bufio.NewScanner(os.Stdin)
+	askForPress()
+	shouldContinue := true
+	for shouldContinue && scanner.Scan() {
+		text := scanner.Text()
+		switch text {
+		case "1":
+			CreateBlog(&client, scanner)
+		case "exit":
+			log.Println("closing blog client")
+			shouldContinue = false
+		default:
+			log.Println("Dhang se daalo!")
+		}
+		if shouldContinue {
+			askForPress()
+		}
+	}
+
+}
+
+func askForPress() {
+	log.Println("Please type the label for its action \n1 :  Create Blog \n2 :  Read Blog \nexit : To close Blog Client")
+}
